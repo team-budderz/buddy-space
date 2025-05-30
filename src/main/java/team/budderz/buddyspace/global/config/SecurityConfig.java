5b355be85af93bb67da94c045c2cd6910710619d
exmrim <exmrim@naver.com>
2025-05-30 17:25:58 +0900

    feat: SecurityConfig 구현

---
+++ src/main/java/team/budderz/buddyspace/global/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package team.budderz.buddyspace.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import team.budderz.buddyspace.global.security.SecurityFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final SecurityFilter securityFilter;
+    //private final CorsConfigurationSource corsConfigurationSource; // 배포 후 설정
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                //.cors(cors -> cors.configurationSource(corsConfigurationSource)) // 배포 후 설정
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("**").permitAll()
+                        //.requestMatchers("/api/users/login","/api/users/signup").permitAll()
+                        //.requestMatchers("**").hasRole("ADMIN") // 테스트용
+                        //.anyRequest().authenticated()
+                        )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable());
+
+                return http.build();
+    }
+}
