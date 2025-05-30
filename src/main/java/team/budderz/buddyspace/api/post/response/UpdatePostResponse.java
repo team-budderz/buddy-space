01716fabaa260f8c75d4d88526858dbc2f3fcb70
dawn0920 <saoul0920@gmail.com>
2025-05-30 20:14:19 +0900

    feat: 응답 DTO에 createdAt과 modifiedAt 필드 추가

--- src/main/java/team/budderz/buddyspace/api/post/response/UpdatePostResponse.java
+++ src/main/java/team/budderz/buddyspace/api/post/response/UpdatePostResponse.java
@@ -3,14 +3,20 @@ package team.budderz.buddyspace.api.post.response;
 import lombok.Getter;
 import team.budderz.buddyspace.infra.database.post.entity.Post;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class UpdatePostResponse {
 
     private final String content;
     private final Boolean isNotice;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
 
     public UpdatePostResponse(Post post) {
         this.content = post.getContent();
         this.isNotice = post.getIsNotice();
+        this.createdAt =  post.getCreatedAt();
+        this.modifiedAt = post.getModifiedAt();
     }
 }
