0cb01b8578c6c669ceddd0a76d36b3441f24e51b
dawn0920 <saoul0920@gmail.com>
2025-06-02 19:38:51 +0900

    chore: posts 테이블 자동생성 ID 반영 및 댓글 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,7 +14,7 @@ VALUES
 -- 2. User 데이터
 INSERT INTO users (name, email, password, birth_date, gender, address, phone, image_url, provider, provider_id, role, neighborhood_id)
 VALUES
-    ('Alice', 'alice@example.com', 'password123', '1995-05-10', 'F', 'Seoul, Gangnam-gu', '010-1234-5678', NULL, 'LOCAL', NULL, 'USER', 1),
+    ('Alice', 'alice@example.com', 'Password123@', '1995-05-10', 'F', 'Seoul, Gangnam-gu', '010-1234-5678', NULL, 'LOCAL', NULL, 'USER', 1),
     ('Bob', 'bob@example.com', 'securepw', '1990-10-22', 'M', 'Busan, Haeundae-gu', '010-8765-4321', NULL, 'KAKAO', 'kakao123', 'USER', NULL);
 
 
@@ -27,7 +27,16 @@ VALUES
 
 -- 4. Post 데이터
 INSERT INTO posts (
-    id, group_id, user_id, content, reserve_at, is_notice)
+    group_id, user_id, content, reserve_at, is_notice)
 VALUES
-    (1, 1, 1, '내용 1번', null, false),
-    (2, 1, 2, '내용 2번', null, false);
+    (1, 1, '내용 1번', null, false),
+    (1, 2, '내용 2번', null, false);
+
+-- 5. Comments 데이터
+INSERT INTO comments (
+    post_id, user_id, content, parent_id
+) VALUES
+      (1, 1, '첫 번째 댓글입니다.', NULL),
+      (1, 2, '첫 번째 댓글에 대한 대댓글입니다.', 1),
+      (2, 1, '두 번째 게시글의 댓글입니다.', NULL),
+      (2, 2, '두 번째 게시글 댓글에 대한 대댓글입니다.', 3);
