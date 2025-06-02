a6cfb8f6de71a05517dfb361bd09a397ea3f29ac
exmrim <exmrim@naver.com>
2025-06-02 12:55:03 +0900

    feat: UserErrorCode - INVALID_USER_PASSWORD 추가

--- src/main/java/team/budderz/buddyspace/domain/user/exception/UserErrorCode.java
+++ src/main/java/team/budderz/buddyspace/domain/user/exception/UserErrorCode.java
@@ -10,7 +10,10 @@ import team.budderz.buddyspace.global.response.ErrorCode;
 public enum UserErrorCode implements ErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"U001","존재하지 않는 유저 입니다."),
-    INVALID_USER_EMAIL(HttpStatus.UNAUTHORIZED.value() ,"U002","유효하지 않은 사용자 이메일입니다.");
+    INVALID_USER_EMAIL(HttpStatus.UNAUTHORIZED.value() ,"U002","유효하지 않은 사용자 이메일입니다."),
+    INVALID_USER_PASSWORD(HttpStatus.UNAUTHORIZED.value() ,"U003","유효하지 않은 사용자 비밀번호입니다.");
+
+
     private final int status;
     private final String code;
     private final String message;
