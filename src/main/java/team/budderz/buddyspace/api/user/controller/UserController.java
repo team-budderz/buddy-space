5456171b42f8d8a3edd0783c9c0ff7482e03d50f
exmrim <exmrim@naver.com>
2025-06-02 11:07:46 +0900

    feat: UserController 추가

---
+++ src/main/java/team/budderz/buddyspace/api/user/controller/UserController.java
@@ -0,0 +1,29 @@
+package team.budderz.buddyspace.api.user.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import team.budderz.buddyspace.api.user.request.SignupRequest;
+import team.budderz.buddyspace.api.user.response.SignupResponse;
+import team.budderz.buddyspace.domain.user.service.UserService;
+import team.budderz.buddyspace.global.response.BaseResponse;
+import team.budderz.buddyspace.global.security.UserAuth;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public BaseResponse<SignupResponse> signup(@Valid @RequestBody SignupRequest signupRequest) {
+        return new BaseResponse<>(userService.signup(signupRequest));
+    }
+
+
+}
