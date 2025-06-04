ef317afedf614a6871d6c87892e9f5f9b382c26c
rang <withghd@gmail.com>
2025-06-04 18:53:40 +0900

    fix: private 생성자 추가

--- src/main/java/team/budderz/buddyspace/domain/group/constant/GroupDefaults.java
+++ src/main/java/team/budderz/buddyspace/domain/group/constant/GroupDefaults.java
@@ -2,7 +2,9 @@ package team.budderz.buddyspace.domain.group.constant;
 
 public final class GroupDefaults {
 
-    public static String DEFAULT_ONLINE_GROUP_COVER_IMAGE_URL = "https://raw.githubusercontent.com/withong/my-storage/main/budderz/icon-online-group.png";
-    public static String DEFAULT_OFFLINE_GROUP_COVER_IMAGE_URL = "https://raw.githubusercontent.com/withong/my-storage/main/budderz/icon-offline-group.png";
-    public static String DEFAULT_HYBRID_GROUP_COVER_IMAGE_URL = "https://raw.githubusercontent.com/withong/my-storage/main/budderz/icon-hybrid-group.png";
+    private GroupDefaults() {}
+
+    public static final String DEFAULT_ONLINE_GROUP_COVER_IMAGE_URL = "https://raw.githubusercontent.com/withong/my-storage/main/budderz/icon-online-group.png";
+    public static final String DEFAULT_OFFLINE_GROUP_COVER_IMAGE_URL = "https://raw.githubusercontent.com/withong/my-storage/main/budderz/icon-offline-group.png";
+    public static final String DEFAULT_HYBRID_GROUP_COVER_IMAGE_URL = "https://raw.githubusercontent.com/withong/my-storage/main/budderz/icon-hybrid-group.png";
 }
