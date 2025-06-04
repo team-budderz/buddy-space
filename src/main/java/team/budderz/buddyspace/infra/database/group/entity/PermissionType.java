7376121f24e54a6ae103870371a5fc2697fc28a5
rang <withghd@gmail.com>
2025-06-04 18:54:36 +0900

    fix: update 권한 추가

--- src/main/java/team/budderz/buddyspace/infra/database/group/entity/PermissionType.java
+++ src/main/java/team/budderz/buddyspace/infra/database/group/entity/PermissionType.java
@@ -2,12 +2,16 @@ package team.budderz.buddyspace.infra.database.group.entity;
 
 public enum PermissionType {
     CREATE_POST,
+    UPDATE_POST,
     DELETE_POST,
     CREATE_SCHEDULE,
+    UPDATE_SCHEDULE,
     DELETE_SCHEDULE,
     CREATE_MISSION,
+    UPDATE_MISSION,
     DELETE_MISSION,
     CREATE_VOTE,
+    UPDATE_VOTE,
     DELETE_VOTE,
     CREATE_CHAT_ROOM
 }
