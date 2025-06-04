ce4e7ab1c6d7f51e451818c0e1a41d8f33da5e86
rang <withghd@gmail.com>
2025-06-04 14:25:30 +0900

    feat: deleteAllByUser_Id 추가

--- src/main/java/team/budderz/buddyspace/infra/database/membership/repository/MembershipRepository.java
+++ src/main/java/team/budderz/buddyspace/infra/database/membership/repository/MembershipRepository.java
@@ -1,7 +1,11 @@
 package team.budderz.buddyspace.infra.database.membership.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import team.budderz.buddyspace.infra.database.membership.entity.Membership;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+
+    @Modifying(clearAutomatically = true)
+    void deleteAllByUser_Id(Long userId);
 }
