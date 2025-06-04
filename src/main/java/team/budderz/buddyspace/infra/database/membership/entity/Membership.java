455ea009a711d49567916bfd8d9f6bdd358e7203
rang <withghd@gmail.com>
2025-06-04 14:36:00 +0900

    fix: Membership entity user, group 필드 추가

--- src/main/java/team/budderz/buddyspace/infra/database/membership/entity/Membership.java
+++ src/main/java/team/budderz/buddyspace/infra/database/membership/entity/Membership.java
@@ -2,6 +2,8 @@ package team.budderz.buddyspace.infra.database.membership.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import team.budderz.buddyspace.infra.database.group.entity.Group;
+import team.budderz.buddyspace.infra.database.user.entity.User;
 
 @Getter
 @Entity
@@ -11,4 +13,12 @@ public class Membership { // 상태 값으로 가입 경로 관리
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id", nullable = false)
+    private Group group;
 }
