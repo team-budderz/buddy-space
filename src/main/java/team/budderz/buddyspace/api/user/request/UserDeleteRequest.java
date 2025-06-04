9e299723a6bb72bb8e96bc4b577eb00172bb7c87
exmrim <exmrim@naver.com>
2025-06-04 14:12:36 +0900

    feat: UserDeleteRequest 수정 - 어노테이션 추가

--- src/main/java/team/budderz/buddyspace/api/user/request/UserDeleteRequest.java
+++ src/main/java/team/budderz/buddyspace/api/user/request/UserDeleteRequest.java
@@ -1,6 +1,9 @@
 package team.budderz.buddyspace.api.user.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record UserDeleteRequest(
+        @NotBlank
         String password
 ) {
 
