f0fa2d61a491e19de75da81ac3fd6ddc2c4a8253
exmrim <exmrim@naver.com>
2025-06-05 16:05:21 +0900

    feat: SecurityConfig 수정 - oauth 설정 추가

--- src/main/java/team/budderz/buddyspace/global/config/SecurityConfig.java
+++ src/main/java/team/budderz/buddyspace/global/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package team.budderz.buddyspace.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
+import team.budderz.buddyspace.global.oauth2.handler.OAuth2SuccessHandler;
+import team.budderz.buddyspace.global.oauth2.service.CustomOAuth2UserService;
 import team.budderz.buddyspace.global.security.SecurityFilter;
 
 @Configuration
@@ -19,6 +22,8 @@ import team.budderz.buddyspace.global.security.SecurityFilter;
 public class SecurityConfig {
 
     private final SecurityFilter securityFilter;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
     //private final CorsConfigurationSource corsConfigurationSource; // 배포 후 설정
 
     @Bean
@@ -32,10 +37,20 @@ public class SecurityConfig {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         //.requestMatchers("**").permitAll()
-                        .requestMatchers("/api/users/login","/api/users/signup").permitAll()
+                        .requestMatchers(
+                                "/api/users/login",
+                                "/api/users/signup",
+                                "/oauth2/**",
+                                "/login/oauth2/**"
+                                ).permitAll()
                         //.requestMatchers("**").hasRole("ADMIN") // 테스트용
                         .anyRequest().authenticated()
                         )
+                .oauth2Login(oauth ->
+                        oauth.userInfoEndpoint(userInfo ->
+                                userInfo.userService(customOAuth2UserService))
+                                .successHandler(oAuth2SuccessHandler)
+                )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(formLogin -> formLogin.disable());
