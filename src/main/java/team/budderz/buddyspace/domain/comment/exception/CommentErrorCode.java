40abaf9cd1e0e569c2ffff34a0b5cbcbedfc3e55
dawn0920 <saoul0920@gmail.com>
2025-06-05 15:50:43 +0900

    refactor: 댓글 도메인 에러 코드 접두어를 'P'에서 'C'로 변경 및 게시글 소속 댓글 검증 오류 코드 추가

--- src/main/java/team/budderz/buddyspace/domain/comment/exception/CommentErrorCode.java
+++ src/main/java/team/budderz/buddyspace/domain/comment/exception/CommentErrorCode.java
@@ -8,12 +8,13 @@ import team.budderz.buddyspace.global.response.ErrorCode;
 @Getter
 @AllArgsConstructor
 public enum CommentErrorCode implements ErrorCode {
-    GROUP_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"P001","존재하지 않는 그룹 입니다.")
-    , USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"P002","존재하지 않는 유저 입니다.")
-    , POST_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"P003","존재하지 않는 게시글 입니다.")
-    , COMMENT_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"P004","존재하지 않는 댓글 입니다.")
-    , UNAUTHORIZED_COMMENT_DELETE(HttpStatus.FORBIDDEN.value(), "P005", "댓글 삭제 권한이 없습니다.")
-    , UNAUTHORIZED_COMMENT_UPDATE(HttpStatus.FORBIDDEN.value(), "P006", "댓글 수정 권한이 없습니다.")
+    GROUP_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"C001","존재하지 않는 그룹 입니다.")
+    , USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"C002","존재하지 않는 유저 입니다.")
+    , POST_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"C003","존재하지 않는 게시글 입니다.")
+    , COMMENT_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value() ,"C004","존재하지 않는 댓글 입니다.")
+    , UNAUTHORIZED_COMMENT_DELETE(HttpStatus.FORBIDDEN.value(), "C005", "댓글 삭제 권한이 없습니다.")
+    , UNAUTHORIZED_COMMENT_UPDATE(HttpStatus.FORBIDDEN.value(), "C006", "댓글 수정 권한이 없습니다.")
+    , COMMENT_NOT_BELONG_TO_POST( HttpStatus.BAD_REQUEST.value(), "C007", "해당 게시글에 속한 댓글이 아닙니다.")
 
     ;
 
