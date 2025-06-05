d0e32dc548a7b54fe2c790c264f891c1a19c9b2a
rang <withghd@gmail.com>
2025-06-05 15:35:35 +0900

    refactor: null 체크 추가

--- src/main/java/team/budderz/buddyspace/infra/database/group/entity/GroupSortType.java
+++ src/main/java/team/budderz/buddyspace/infra/database/group/entity/GroupSortType.java
@@ -10,6 +10,9 @@ public enum GroupSortType {
     LATEST;
 
     public static GroupSortType from(String value) {
+        if (value == null) {
+            throw new GroupException(GroupErrorCode.INVALID_GROUP_SORT_TYPE);
+        }
         return Arrays.stream(values())
                 .filter(e -> e.name().equalsIgnoreCase(value))
                 .findFirst()
