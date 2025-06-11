6dbdda182bf9a8798608cd53a3821ee6ef443b16
exmrim <exmrim@naver.com>
2025-06-11 15:35:43 +0900

    feat: findAllByMission_Group_IdAndMission_Id() 추가

--- src/main/java/team/budderz/buddyspace/infra/database/mission/repository/MissionPostRepository.java
+++
@@ -1,7 +0,0 @@
-package team.budderz.buddyspace.infra.database.mission.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team.budderz.buddyspace.infra.database.mission.entity.MissionPost;
-
-public interface MissionPostRepository extends JpaRepository<MissionPost, Long> {
-}
