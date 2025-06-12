7aecb7d0a3a042610d96ff68ece511eb6cb94f9f
exmrim <exmrim@naver.com>
2025-06-12 16:57:47 +0900

    feat: 필드 수정

--- src/main/java/team/budderz/buddyspace/infra/database/neighborhood/entity/Neighborhood.java
+++ src/main/java/team/budderz/buddyspace/infra/database/neighborhood/entity/Neighborhood.java
@@ -1,8 +1,10 @@
 package team.budderz.buddyspace.infra.database.neighborhood.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import team.budderz.buddyspace.global.entity.BaseEntity;
 import team.budderz.buddyspace.infra.database.user.entity.User;
 
@@ -17,14 +19,14 @@ public class Neighborhood extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String city_name;
+    @Column(nullable = true)
+    private String cityName;
 
-    @Column(nullable = false)
-    private String district_name;
+    @Column(nullable = true)
+    private String districtName;
 
-    @Column(nullable = false)
-    private String ward_name;
+    @Column(nullable = true)
+    private String wardName;
 
     @Column(precision = 9, scale = 6)
     private BigDecimal lat;
@@ -32,16 +34,12 @@ public class Neighborhood extends BaseEntity {
     @Column(precision = 9, scale = 6)
     private BigDecimal lng;
 
-    @Column(nullable = false)
-    private boolean isVerified;
-
-    public Neighborhood(String city_name, String district_name, String ward_name, BigDecimal lat, BigDecimal lng, boolean isVerified) {
-        this.city_name = city_name;
-        this.district_name = district_name;
-        this.ward_name = ward_name;
+    @Builder
+    public Neighborhood(String cityName, String districtName, String wardName, BigDecimal lat, BigDecimal lng) {
+        this.cityName = cityName;
+        this.districtName = districtName;
+        this.wardName = wardName;
         this.lat = lat;
         this.lng = lng;
-        this.isVerified = isVerified;
     }
-
 }
