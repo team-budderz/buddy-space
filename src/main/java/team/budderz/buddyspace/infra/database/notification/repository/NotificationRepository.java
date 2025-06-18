0807097d0b8a71f0e3d3c11bd8802a34d74a4384
dawn0920 <saoul0920@gmail.com>
2025-06-18 20:49:59 +0900

    feat: NotificationRepository 구현

--- src/main/java/team/budderz/buddyspace/infra/database/notification/repository/NotificationRepository.java
+++ src/main/java/team/budderz/buddyspace/infra/database/notification/repository/NotificationRepository.java
@@ -1,7 +1,11 @@
 package team.budderz.buddyspace.infra.database.notification.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.budderz.buddyspace.infra.database.notification.entity.Notification;
+import team.budderz.buddyspace.infra.database.user.entity.User;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
+    Page<Notification> findAllByUserOrderByCreatedAtDesc(User user, Pageable pageable);
 }
