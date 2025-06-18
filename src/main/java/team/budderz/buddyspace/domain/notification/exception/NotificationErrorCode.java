dcd524488fa615c0e535c3d52e2e8bfc97152a35
dawn0920 <saoul0920@gmail.com>
2025-06-18 20:50:21 +0900

    feat: NotificationErrorCode에 USER_ID_NOT_FOUND 추가

--- src/main/java/team/budderz/buddyspace/domain/notification/exception/NotificationErrorCode.java
+++ src/main/java/team/budderz/buddyspace/domain/notification/exception/NotificationErrorCode.java
@@ -8,8 +8,8 @@ import team.budderz.buddyspace.global.response.ErrorCode;
 @Getter
 @AllArgsConstructor
 public enum NotificationErrorCode implements ErrorCode {
-    UNSUPPORTED_NOTIFICATION_TYPE(HttpStatus.BAD_REQUEST.value(), "N001", "지원하지 않는 알림 타입입니다.");
-
+    UNSUPPORTED_NOTIFICATION_TYPE(HttpStatus.BAD_REQUEST.value(), "N001", "지원하지 않는 알림 타입입니다.")
+    , USER_ID_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "N002", "존재하지 않는 유저입니다.")
     ;
 
 
