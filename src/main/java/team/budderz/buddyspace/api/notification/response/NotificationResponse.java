9c8c9ca97b49aabdc84a03443d1166718521d7da
dawn0920 <saoul0920@gmail.com>
2025-06-19 15:11:59 +0900

    refactor: NotificationResponse 필드 구조 변경 및 정적 팩토리 메서드 추가

--- src/main/java/team/budderz/buddyspace/api/notification/response/NotificationResponse.java
+++ src/main/java/team/budderz/buddyspace/api/notification/response/NotificationResponse.java
@@ -1,5 +1,6 @@
 package team.budderz.buddyspace.api.notification.response;
 
+import team.budderz.buddyspace.infra.database.notification.entity.Notification;
 import team.budderz.buddyspace.infra.database.notification.entity.NotificationType;
 
 import java.time.LocalDateTime;
@@ -7,12 +8,21 @@ import java.time.LocalDateTime;
 public record NotificationResponse(
         Long notificationId,
         String content,
-        String previewContent,
-        String groupName,
-        String senderName,
-        String senderImageUrl,
-        boolean isRead,
         String url,
+        boolean isRead,
+        String groupName,
         LocalDateTime createdAt,
         NotificationType type
-) {}
+) {
+    public static NotificationResponse from(Notification notification) {
+        return new NotificationResponse(
+                notification.getId(),
+                notification.getContent(),
+                notification.getUrl(),
+                notification.isRead(),
+                notification.getGroup().getName(),
+                notification.getCreatedAt(),
+                notification.getType()
+        );
+    }
+}
