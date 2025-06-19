2be7155d346db90409833ac083b7cc2d740aea1b
dawn0920 <saoul0920@gmail.com>
2025-06-19 15:13:45 +0900

    feat: 알림 서비스 기능 확장 (조회, 읽음 처리, 미읽음 개수 조회)

--- src/main/java/team/budderz/buddyspace/domain/notification/service/NotificationService.java
+++ src/main/java/team/budderz/buddyspace/domain/notification/service/NotificationService.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import team.budderz.buddyspace.api.notification.response.FindsNotificationResponse;
 import team.budderz.buddyspace.api.notification.response.NotificationResponse;
 import team.budderz.buddyspace.domain.notification.exception.NotificationErrorCode;
 import team.budderz.buddyspace.global.exception.BaseException;
@@ -21,16 +22,36 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final UserRepository userRepository;
 
+    // 알림 보내기
+
+    // 알림 모음
     @Transactional(readOnly = true)
-    public Page<NotificationResponse> findsNotice(
+    public Page<FindsNotificationResponse> findsNotice(
             Long userId,
             Pageable pageable
     ) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new BaseException(NotificationErrorCode.USER_ID_NOT_FOUND));
 
-        Page<Notification> notifications = notificationRepository.findAllByUserOrderByCreatedAtDesc(user, pageable);
-        return  notifications
-                .map(NotificationResponse::from);
+        return notificationRepository.findAllByUserOrderByCreatedAtDesc(user, pageable)
+                .map(FindsNotificationResponse::from);
+    }
+
+    // 알림 읽음 처리
+    @Transactional
+    public void readNotification(Long notificationId, Long userId) {
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(() -> new BaseException(NotificationErrorCode.NOTIFICATION_NOT_FOUND));
+
+        if(notification.doesNotBelongToUser(userId)) {
+            throw new BaseException(NotificationErrorCode.NO_AUTH_TO_READ_NOTIFICATION);
+        }
+        notification.markAsRead();
+    }
+
+    // 알림 개수 조회
+    @Transactional(readOnly = true)
+    public Long countUnreadNotifications(Long userId) {
+        return notificationRepository.countByUserIdAndIsReadFalse(userId);
     }
 }
