13d8035a66082c499788ebc185d7cd7d56bd40b8
dawn0920 <saoul0920@gmail.com>
2025-06-19 17:31:13 +0900

    refactor: NotificationArgs에 groupName 필드 추가

--- src/main/java/team/budderz/buddyspace/api/notification/response/NotificationArgs.java
+++ src/main/java/team/budderz/buddyspace/api/notification/response/NotificationArgs.java
@@ -2,6 +2,7 @@ package team.budderz.buddyspace.api.notification.response;
 
 public record NotificationArgs(
         String senderName,
+        String groupName,
         String content,
         Long targetId
 ) {
