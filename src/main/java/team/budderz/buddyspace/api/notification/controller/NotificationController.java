f979e1c39a18cabcb95d61d24d60ada075534945
dawn0920 <saoul0920@gmail.com>
2025-06-19 17:26:50 +0900

    feat: 알림 서비스 기능 확장 (조회, 읽음 처리, 미읽음 개수 조회) refactor: 알림 전체 조회 응답 타입을 FindsNotificationResponse로 수정

--- src/main/java/team/budderz/buddyspace/api/notification/controller/NotificationController.java
+++ src/main/java/team/budderz/buddyspace/api/notification/controller/NotificationController.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import team.budderz.buddyspace.api.notification.response.NotificationResponse;
+import team.budderz.buddyspace.api.notification.response.FindsNotificationResponse;
 import team.budderz.buddyspace.domain.notification.service.NotificationService;
 import team.budderz.buddyspace.global.response.BaseResponse;
 import team.budderz.buddyspace.global.security.UserAuth;
@@ -19,18 +19,31 @@ public class NotificationController {
 
     // 알림 전체 조회
     @GetMapping
-    public BaseResponse<Page<NotificationResponse>> findsNotice (
+    public BaseResponse<Page<FindsNotificationResponse>> findsNotice (
             @AuthenticationPrincipal UserAuth userAuth,
             Pageable pageable
     ) {
-        Long userId = userAuth.getUserId();
-        Page<NotificationResponse> response = notificationService.findsNotice(userId, pageable);
+        Page<FindsNotificationResponse> response = notificationService.findsNotice(userAuth.getUserId(), pageable);
         return new BaseResponse<>(response);
     }
 
     // 알림 읽음 처리
-
+    @PatchMapping("/{notificationId}/read")
+    public BaseResponse<Void> readNotification(
+            @PathVariable Long notificationId,
+            @AuthenticationPrincipal UserAuth userAuth
+    ) {
+        notificationService.readNotification(notificationId, userAuth.getUserId());
+        return new BaseResponse<>(null);
+    }
 
     // 알림 개수 조회
+    @GetMapping("/notice-count")
+    public BaseResponse<Long> countUnreadNotifivation(
+            @AuthenticationPrincipal UserAuth userAuth
+    ) {
+        Long count = notificationService.countUnreadNotifications(userAuth.getUserId());
+        return new BaseResponse<>(count);
+    }
 
 }
