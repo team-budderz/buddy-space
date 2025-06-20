7db189d7335bf4b4df1b35f8e18998e0299bc991
exmrim <exmrim@naver.com>
2025-06-20 17:59:11 +0900

    feat: SignupRequestDto 항목 삭제

--- src/main/java/team/budderz/buddyspace/api/user/request/SignupRequest.java
+++ src/main/java/team/budderz/buddyspace/api/user/request/SignupRequest.java
@@ -28,10 +28,6 @@ public record SignupRequest(
         String address,
         @NotBlank
         @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다.")
-        String phone,
-        @NotNull
-        UserProvider provider,
-        @NotNull
-        UserRole role
+        String phone
 ) {
 }
