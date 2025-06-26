4b66cdf6caf6c924fbdb4ccace6eab20622c5693
rang <withghd@gmail.com>
2025-06-27 03:44:38 +0900

    chore: 테스트 화면 경로 인가 설정

--- src/main/java/team/budderz/buddyspace/global/config/SecurityConfig.java
+++ src/main/java/team/budderz/buddyspace/global/config/SecurityConfig.java
@@ -42,6 +42,8 @@ public class SecurityConfig {
                                 "/test-websocket.html",
                                 "/test-post-attachment.html",
                                 "/test/**",
+                                "/js/**",
+                                "/css/**",
                                 "/ws/**",
                                 "/static/**",
                                 "/api/users/login",
