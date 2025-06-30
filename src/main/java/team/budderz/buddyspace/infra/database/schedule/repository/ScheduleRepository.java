95d168cf7d531c003d2e8f85e4d7da0862740a17
정소민 <yjk70702002@gmail.com>
2025-06-30 21:18:48 +0900

    refactor: 일정 조회 fetch join 설정

--- src/main/java/team/budderz/buddyspace/infra/database/schedule/repository/ScheduleRepository.java
+++ src/main/java/team/budderz/buddyspace/infra/database/schedule/repository/ScheduleRepository.java
@@ -1,15 +1,19 @@
 package team.budderz.buddyspace.infra.database.schedule.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import team.budderz.buddyspace.infra.database.schedule.entity.Schedule;
-
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import team.budderz.buddyspace.infra.database.schedule.entity.Schedule;
+import team.budderz.buddyspace.infra.database.vote.entity.Vote;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 	@Query(""" 
-		SELECT s FROM Schedule s
+		SELECT s
+		FROM Schedule s JOIN FETCH s.author
 		WHERE s.startAt <= :monthEnd
 		AND s.endAt >= :monthStart
 		AND s.group.id = :groupId
@@ -19,4 +23,12 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 	List<Schedule> findAllByMonth(Long groupId, LocalDateTime monthStart, LocalDateTime monthEnd);
 
 	void deleteAllByGroup_Id(Long groupId);
+
+	@Query("""
+		SELECT s
+		FROM Schedule s
+		JOIN FETCH s.author
+		WHERE s.id = :scheduleId
+	""")
+	Optional<Schedule> findById(Long scheduleId);
 }
