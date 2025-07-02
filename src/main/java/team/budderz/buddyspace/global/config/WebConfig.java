26c5161457e35b8ad2412e6812cedf87a67c884c
rang <withghd@gmail.com>
2025-07-02 21:05:29 +0900

    chore: WebConfig 설정 추가

---
+++ src/main/java/team/budderz/buddyspace/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package team.budderz.buddyspace.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/test/**")
+                .addResourceLocations("classpath:/static/test/");
+
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+    }
+}
