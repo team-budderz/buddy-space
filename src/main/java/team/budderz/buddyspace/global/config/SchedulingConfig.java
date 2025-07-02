b3828a90f0a4f6cdac013c889971bfe1d688c776
rang <withghd@gmail.com>
2025-07-02 20:41:05 +0900

    feat: 고아 첨부파일 삭제 스케줄 적용

---
+++ src/main/java/team/budderz/buddyspace/global/config/SchedulingConfig.java
@@ -0,0 +1,10 @@
+package team.budderz.buddyspace.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+    // 스케줄링 기능 활성화
+}
