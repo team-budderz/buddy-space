5a466081eeed20c74d56c1de25d9fa1745fd09f2
rang <withghd@gmail.com>
2025-07-03 20:46:13 +0900

    refactor: 식별자 추가 및 코드 정렬

--- src/main/java/team/budderz/buddyspace/api/mission/response/MissionResponse.java
+++ src/main/java/team/budderz/buddyspace/api/mission/response/MissionResponse.java
@@ -11,13 +11,27 @@ public record MissionResponse(
         @Schema(description = "미션 식별자", example = "3")
         Long missionId,
 
-        @Schema(description = "미션 제목", example = "코드카타") String title,
-        @Schema(description = "미션 설명", example = "1일 1회 알고리즘 문제 풀이") String description,
-        @Schema(description = "미션 시작 날짜", example = "2025-05-27") String startedAt,
-        @Schema(description = "미션 끝나는 날짜", example = "2025-07-07") String endedAt,
-        @Schema(description = "미션 빈도", example = "30") Integer frequency,
-        @Schema(description = "미션 진행된 일수", example = "2") int progressDay,
-        @Schema(description = "미션 생성자 이름", example = "홍길동") String authorName
+        @Schema(description = "미션 제목", example = "코드카타")
+        String title,
+
+        @Schema(description = "미션 설명", example = "1일 1회 알고리즘 문제 풀이")
+        String description,
+
+        @Schema(description = "미션 시작 날짜", example = "2025-05-27")
+        String startedAt,
+
+        @Schema(description = "미션 끝나는 날짜", example = "2025-07-07")
+        String endedAt,
+
+        @Schema(description = "미션 빈도", example = "30")
+        Integer frequency,
+
+        @Schema(description = "미션 진행된 일수", example = "2")
+        int progressDay,
+
+        @Schema(description = "미션 생성자 이름", example = "홍길동")
+        String authorName
+
 ) {
     public static MissionResponse from(Mission mission) {
         LocalDate today = LocalDate.now();
