cbbc54bb7d22b51694437d8b31e8fd9750d4f88b
rang <withghd@gmail.com>
2025-07-07 01:14:36 +0900

    fix: 첨부파일 삭제 시 연관 정보 검증하도록 수정

--- src/main/java/team/budderz/buddyspace/infra/database/attachment/repository/PostAttachmentRepository.java
+++ src/main/java/team/budderz/buddyspace/infra/database/attachment/repository/PostAttachmentRepository.java
@@ -1,6 +1,7 @@
 package team.budderz.buddyspace.infra.database.attachment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.budderz.buddyspace.infra.database.attachment.entity.Attachment;
 import team.budderz.buddyspace.infra.database.attachment.entity.PostAttachment;
 import team.budderz.buddyspace.infra.database.post.entity.Post;
 
@@ -9,6 +10,10 @@ import java.util.List;
 public interface PostAttachmentRepository extends JpaRepository<PostAttachment, Long> {
 
     void deleteAllByPost(Post post);
+
+    boolean existsAllByAttachment(Attachment attachment);
+
+    void deleteByAttachment(Attachment attachment);
 
     List<PostAttachment> findAllByPost(Post post);
 
