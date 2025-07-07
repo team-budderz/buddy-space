4aeabb55512efed0b920e77aa0e697b5d17fcc5d
dawn0920 <saoul0920@gmail.com>
2025-07-07 17:19:06 +0900

    chore(security): STOMP 경로 허용 범위를 /ws 로 확장

--- src/main/java/team/budderz/buddyspace/global/config/SecurityConfig.java
+++ src/main/java/team/budderz/buddyspace/global/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                                 "/test/**",
                                 "/js/**",
                                 "/css/**",
+                                "/ws",
                                 "/ws/**",
                                 "/pub/**",
                                 "/sub/**"
