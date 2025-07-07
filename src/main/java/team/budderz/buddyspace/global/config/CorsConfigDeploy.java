7f9fd7e94d824ea9eb671262a3a7c413eea45533
정소민 <yjk70702002@gmail.com>
2025-07-07 20:33:33 +0900

    feat: cors 설정

--- src/main/java/team/budderz/buddyspace/global/config/CorsConfigDeploy.java
+++ src/main/java/team/budderz/buddyspace/global/config/CorsConfigDeploy.java
@@ -6,24 +6,37 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @Profile("deploy")
-public class CorsConfigDeploy {
+public class CorsConfigDeploy implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://budderz.co.kr", "https://buddy-space-front-hwj4.vercel.app"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "PATCH", "OPTIONS"));
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-        config.setAllowCredentials(true);
-        config.setMaxAge(3600L); // 1시간 캐싱
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("https://budderz.co.kr", "https://api.budderz.co.kr")
+            .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+            .exposedHeaders("Authorization", "Set-Cookie")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
+
+    // @Bean
+    // public CorsConfigurationSource corsConfigurationSource() {
+    //     CorsConfiguration config = new CorsConfiguration();
+    //     config.setAllowedOrigins(List.of("https://budderz.co.kr", "https://buddy-space-front-hwj4.vercel.app"));
+    //     config.setAllowedMethods(List.of("GET","POST","PUT","DELETE", "PATCH", "OPTIONS"));
+    //     config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    //     config.setAllowCredentials(true);
+    //     config.setMaxAge(3600L); // 1시간 캐싱
+    //
+    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    //     source.registerCorsConfiguration("/**", config);
+    //     return source;
+    // }
 }
